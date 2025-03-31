942307f735bffb83cbbc5c00f75850b8d917f2dd
Aniket Ingole <aniketingolenet@gmail.com>
2025-03-31 16:35:28 +0530

    Added aws config

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -5,3 +5,9 @@ dotenv.config();
 export const PORT = process.env.PORT;
 
 export const DB_URL = process.env.DB_URL;
+
+export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
+
+export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+
+export const AWS_REGION = process.env.AWS_REGION;
